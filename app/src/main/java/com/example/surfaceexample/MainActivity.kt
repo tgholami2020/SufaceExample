db52b63a43dc8c69b5f56134d9f49487f566a2bb
tghoami2020 <tahere.gholaami67@gmail.com>
2023-04-08 09:16:14 -0700

    Surface with Row and Column worked

--- app/src/main/java/com/example/surfaceexample/MainActivity.kt
+++ app/src/main/java/com/example/surfaceexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.surfaceexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.surfaceexample.ui.theme.SurfaceExampleTheme
@@ -19,34 +21,62 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SurfaceExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(                  // Surface can have one child directly
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Green
-                ) {
-                    Row (
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                            ){
-                        HorizontalBar(color = Color.Red)
-                        HorizontalBar(color = Color.Yellow)
-                        HorizontalBar(color = Color.Blue)
-                    }
-                }
-
-            }
+           MainScreen()
         }
     }
 }
 @Composable
-fun HorizontalBar(color: Color){
+fun MainScreen(){
+    Surface (
+        color = Color.Cyan,
+        modifier = Modifier.fillMaxSize()
+            ){
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                ImageSection(resource = R.drawable.mint)
+                ImageSection(resource = R.drawable.keratin)
+                
+            }
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+               ImageSection(resource = R.drawable.argan)
+                ImageSection(resource = R.drawable.pepper)
+            }
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ImageSection(resource = R.drawable.keratin)
+                ImageSection(resource = R.drawable.wheat_q10)
+            }
+        }
+
+    }
+}
+@Composable
+fun ImageSection(resource:Int){
+    Surface(modifier = Modifier
+        .height(120.dp)
+        .width(120.dp)
+
+    ) {
+        Image(painter = painterResource(id = resource), contentDescription =null )
+    }
+
+
+}
+@Composable
+fun SquareBar(color: Color){
     Surface (
         color = color,
         modifier = Modifier
-            .height(400.dp)
-            .width(50.dp)
+            .height(120.dp)
+            .width(120.dp)
     ){
     }
 
